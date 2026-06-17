d1bb7a6d8d2639fe6e9ca62faf03f3efc13f42d0
elise-Iss15 <e.ishimwe3@alustudent.com>
2026-06-17 13:48:47 +0200

    feat: add Task model with title and isDone properties

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'pages/todo_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: const Color(0xFF7F77DD),
+        useMaterial3: true,
+      ),
+      home: const TodoPage(),
+    );
+  }
+}
